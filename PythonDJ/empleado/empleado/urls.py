864e4ece5bbc7b3ea67c35616a2449b8ec591482
TylerCamilo1 <4ckamilo27@gmail.com>
2022-01-03 21:01:03 -0500

    Actulizacion a modificacion en setting.py y creacion de carpeta para base/local/prod

--- PythonDJ/empleado/empleado/urls.py
+++ PythonDJ/empleado/empleado/urls.py
@@ -16,6 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+def PruebaUrl(self):
+    print('===================estamos probando URLS.py')
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('prueba/', PruebaUrl),
+
 ]
