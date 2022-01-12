3e11457b4b310ac17cc0b97ce3990e6a6adb8da1
TylerCamilo1 <4ckamilo27@gmail.com>
2022-01-11 21:43:11 -0500

    Actualizacion a clase 24, funcional!

--- PythonDJ/empleado/applications/persona/models.py
+++ PythonDJ/empleado/applications/persona/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.db.models.fields import CharField
+from applications.departamento.models import Departamento
 
 # Create your models here.
+ 
+class Empleado(models.Model):
+     """Modelo para tabla empleado"""
+     jobChoices= (('0','Contador'),('1','Administrador'),
+                    ('2', 'Economista'),('3','Otro'))
+     #contador,administrador, economista, otro
+     first_name = models.CharField("Nombres", max_length=60)
+     last_name = models.CharField("Apellidos", max_length=60)
+     job = models.CharField("Trabajo", max_length=60, choices=jobChoices)
+     departamento = models.ForeignKey(Departamento, on_delete=models.CASCADE)
+     #image = models.ImageField (upload_to=None, height_field= None,width_field=None, max_lenth=None)
+
+     def __str__(self):
+        return str(self.id) + ' - ' + self.first_name + ' - ' + self.last_name
+    
