f136b5b9bc36494e1bee2e809e146d897dfc500f
TylerCamilo1 <4ckamilo27@gmail.com>
2022-02-17 21:10:49 -0500

    Actualzacion, retomando

--- PythonDJ/empleado/applications/departamento/models.py
+++ PythonDJ/empleado/applications/departamento/models.py
@@ -7,11 +7,11 @@ class Departamento(models.Model):
     short_name = models.CharField('Nombre Corto', max_length = 20,unique=True)
     anulate = models.BooleanField('Anulado', default = False)
     
-    class Meta:
+    class Meta: #Decorador u ordenador
         verbose_name = 'Mi departamento'
         verbose_name_plural = 'Areas de la empresa'
         ordering =['name'] #ordenar
-        unique_together = ('name', 'short_name')
+        unique_together = ('name', 'short_name') #no permite registro repetido
         
     def __str__(self):
         return str(self.id) + '-' + self.name + '-' + self.short_name 
